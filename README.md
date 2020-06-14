9a52cc63669167080cb48c050e09998d46f376e3
Bhaskar Mookerji <mookerji@gmail.com>
2020-06-13 20:18:02 -0700

    Renaming

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ## `h3-rs`: Rust Bindings to h3
 
-Rust bindings to [H3][h3_uber], a C library for hexagonal, hierarchical geospatial
-indexing.
+Rust bindings to [H3][h3_uber], a C library for hexagonal, hierarchical
+geospatial indexing. `h3-rs` interoperates between GeoJSON types define the
+[`geo-types` crate][geo_types]
 
 This is **experimental, in-progress software**.
 
@@ -26,14 +27,26 @@ h3-rs = { version = "0.1.0", path = "../h3-rs" }
 `h3-rs` requires that you already have `h3` and its headers installed on your
 system. Follow those instructions [from h3][h3_install].
 
+## Usage
+
+### CLI
+
+```
+```
+
+### Library
+
+```
+```
+
 ## License
 
-Copyright (c) 2016-2019 Uber Technologies, Inc.
-
-Copyright (c) 2019 Bhaskar Mookerji
+Copyright (c) 2016-2020 Uber Technologies, Inc.
+Copyright (c) 2020 Bhaskar Mookerji
 
 Distributed under the Apache License 2.0
 
 [h3_uber]: https://uber.github.io/h3/#/
 [h3_install]: https://github.com/uber/h3#installing
 [crates_h3_rs]: https://github.com/mookerji/h3-rs
+[geo_types]: https://docs.rs/geo-types/
--- src/hierarchy.rs
+++ src/hierarchy.rs
@@ -1,5 +1,5 @@
-// Copyright 2016-2019 Uber Technologies, Inc.
-// Copyright 2019      Bhaskar Mookerji
+// Copyright 2016-2020 Uber Technologies, Inc.
+// Copyright 2020      Bhaskar Mookerji
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,72 +19,33 @@
 //! functions produce parent (coarser) or children (finer) cells.
 
 use crate::errors::*;
+use crate::region::*;
 use crate::resolution::*;
 use crate::types::*;
 
+use geo_types::Polygon;
+
 impl H3Index {
     /// Returns the parent (or grandparent, etc) hexagon of the given hexagon
-    pub fn get_parent(&self, res: GridResolution) -> H3Index {
+    pub fn parent(&self, res: GridResolution) -> H3Index {
         unsafe { H3Index(h3_sys::h3ToParent(self.0, res as i32)) }
     }
 
     /// Returns the maximum number of children (or grandchildren, etc) that
     /// could be for a given H3Index
-    pub fn get_max_children(&self, child_res: GridResolution) -> usize {
+    pub fn max_children(&self, child_res: GridResolution) -> usize {
         unsafe { h3_sys::maxH3ToChildrenSize(self.0, child_res as i32) as usize }
     }
 
     /// Returns the children for a given H3Index
-    pub fn get_children(&self, child_res: GridResolution) -> Vec<H3Index> {
-        let num_children = self.get_max_children(child_res);
+    pub fn children(&self, child_res: GridResolution) -> Vec<H3Index> {
+        let num_children = self.max_children(child_res);
         let mut buf = Vec::<H3Index>::with_capacity(num_children);
         let ptr = buf.as_mut_ptr();
         unsafe {
             std::mem::forget(buf);
             h3_sys::h3ToChildren(self.0, child_res as i32, ptr as *mut h3_sys::H3Index);
             Vec::from_raw_parts(ptr, num_children, num_children)
-        }
-    }
-}
-
-/// Compacts the set set indexes as best as possible, into the array
-/// compactedSet. compactedSet must be at least the size of h3Set in case the
-/// set cannot be compacted.
-pub fn compact(set: &Vec<H3Index>) -> Result<Vec<H3Index>> {
-    let mut buf = Vec::<H3Index>::with_capacity(set.len());
-    let ptr = buf.as_mut_ptr();
-    unsafe {
-        std::mem::forget(buf);
-        let err = h3_sys::compact(
-            set.as_ptr() as *const h3_sys::H3Index,
-            ptr as *mut h3_sys::H3Index,
-            set.len() as i32,
-        );
-        if err == 0 {
-            Ok(Vec::from_raw_parts(ptr, set.len(), set.len()))
-        } else {
-            Err(Error::UnableToCompact(set.clone()))
-        }
-    }
-}
-
-/// Uncompacts the set compactedSet of indexes to the resolution res
-pub fn uncompact(compacted: &Vec<H3Index>, res: GridResolution) -> Result<Vec<H3Index>> {
-    let max_size = uncompact_size(&compacted, res);
-    let mut buf = Vec::<H3Index>::with_capacity(max_size);
-    let ptr = buf.as_mut_ptr();
-    unsafe {
-        let err = h3_sys::uncompact(
-            compacted.as_ptr() as *const h3_sys::H3Index,
-            compacted.len() as i32,
-            ptr as *mut h3_sys::H3Index,
-            max_size as i32,
-            res as i32,
-        );
-        if err == 0 {
-            Ok(Vec::from_raw_parts(ptr, max_size, max_size))
-        } else {
-            Err(Error::UnableToCompact(compacted.clone()))
         }
     }
 }
@@ -100,18 +61,76 @@ fn uncompact_size(set: &Vec<H3Index>, res: GridResolution) -> usize {
     }
 }
 
+pub trait ToCompactH3Region {
+    /// Compacts the set indexes as best as possible.
+    fn compact(&self) -> Result<Vec<H3Index>>;
+}
+
+pub trait FromCompactH3Region {
+    /// Uncompacts the set of indexes to the resolution
+    fn uncompact(&self, res: GridResolution) -> Result<Vec<H3Index>>;
+}
+
+impl ToCompactH3Region for Vec<H3Index> {
+    fn compact(&self) -> Result<Vec<H3Index>> {
+        let mut buf = Vec::<H3Index>::with_capacity(self.len());
+        let ptr = buf.as_mut_ptr();
+        unsafe {
+            std::mem::forget(buf);
+            let err = h3_sys::compact(
+                self.as_ptr() as *const h3_sys::H3Index,
+                ptr as *mut h3_sys::H3Index,
+                self.len() as i32,
+            );
+            if err == 0 {
+                Ok(Vec::from_raw_parts(ptr, self.len(), self.len()))
+            } else {
+                Err(Error::UnableToCompact(self.clone()))
+            }
+        }
+    }
+}
+
+impl ToCompactH3Region for Polygon<f64> {
+    fn compact(&self) -> Result<Vec<H3Index>> {
+        let res = GridResolution::Z9;
+        self.polyfill(res).compact()
+    }
+}
+
+impl FromCompactH3Region for Vec<H3Index> {
+    fn uncompact(&self, res: GridResolution) -> Result<Vec<H3Index>> {
+        let max_size = uncompact_size(&self, res);
+        let mut buf = Vec::<H3Index>::with_capacity(max_size);
+        let ptr = buf.as_mut_ptr();
+        unsafe {
+            let err = h3_sys::uncompact(
+                self.as_ptr() as *const h3_sys::H3Index,
+                self.len() as i32,
+                ptr as *mut h3_sys::H3Index,
+                max_size as i32,
+                res as i32,
+            );
+            if err == 0 {
+                Ok(Vec::from_raw_parts(ptr, max_size, max_size))
+            } else {
+                Err(Error::UnableToCompact(self.clone()))
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::region::*;
     use geo_types::polygon;
 
     #[test]
     fn test_index_children() {
         let index = H3Index(0x87283472bffffff);
-        let z7_children = index.get_children(GridResolution::Z7);
+        let z7_children = index.children(GridResolution::Z7);
         assert_eq!(z7_children.len(), 1);
-        let z8_children = index.get_children(GridResolution::Z8);
+        let z8_children = index.children(GridResolution::Z8);
         assert_eq!(z8_children.len(), 7);
     }
 
@@ -129,10 +148,9 @@ mod tests {
             interiors: [],
         );
         let res = GridResolution::Z9;
-        let indices = poly.polyfill(res);
-        let compact_hexes = compact(&indices).unwrap();
+        let compact_hexes = poly.polyfill(res).compact().unwrap();
         assert_eq!(compact_hexes.len(), 209);
-        let uncompact_hexes = uncompact(&compact_hexes, res).unwrap();
+        let uncompact_hexes = compact_hexes.uncompact(res).unwrap();
         assert_eq!(uncompact_hexes.len(), 1253);
     }
 }
